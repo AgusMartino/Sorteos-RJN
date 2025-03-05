22387e838fe347a5b2f29ebc92546fcfa3ade8e5
AgusMartino <martinoagustin4@gmail.com>
2025-03-05 18:23:56 -0300

    layout base

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import InitColorSchemeScript from "@mui/joy/InitColorSchemeScript";
 import { CssVarsProvider } from "@mui/joy/styles";
 import CssBaseline from "@mui/joy/CssBaseline";
@@ -6,42 +8,26 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Dioses Training",
-  description: "",
+  title: "Sorteos RJN",
+  description: "Creado para Rojanelos ❤️",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es" suppressHydrationWarning={true}>
+      <Analytics />
       <body
-        className={`${inter.className} max-w-screen flex flex-col min-h-screen`}
+        className={`${inter.className} max-w-screen flex min-h-screen flex-col`}
       >
         <InitColorSchemeScript />
         <CssVarsProvider defaultMode="dark">
           <CssBaseline />
-
-          {/* Contenedor principal */}
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              minHeight: "100vh", // Asegura que la página ocupe toda la altura de la pantalla
-            }}
-          >
-            {/* Contenido principal */}
-            <main
-              style={{
-                flex: 1, // Hace que el contenido ocupe el espacio restante
-              }}
-            >
-              {children}
-            </main>
-          </div>
+          <Navbar />
+          {children}
         </CssVarsProvider>
       </body>
     </html>
